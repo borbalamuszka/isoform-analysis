c206b2148e5fb2136ac46b430b8aa140ee335b06
jameswilsenach <vilsnk@gmail.com>
2026-07-09 13:22:53 +0100

    Change to pipeline runner

--- isoform_dashboard/assets/pipeline_runner.py
+++ isoform_dashboard/assets/pipeline_runner.py
@@ -1,7 +1,7 @@
 import subprocess
 import sys
 
-cmd1 = ['C:\\Users\\vilsn\\Documents\\isoform-analysis\\isoform_dashboard_env\\Scripts\\python.exe', '-u', '-m', 'isoform_distribution.distributions', '--matrix', 'C:\\Users\\vilsn\\Documents\\isoform-analysis\\data\\expressed_isoforms_matrix.tsv', '--gtf', 'C:\\Users\\vilsn\\Documents\\isoform-analysis\\data\\expressed_isoforms.gtf', '--meta-file', 'C:\\Users\\vilsn\\Documents\\isoform-analysis\\data\\metadata.tsv', '--output-dir', 'data/isoform_distributions', '--meta-sample-col', 'sample_id', '--meta-group-col', 'region', '--cutoff-pct', '1.5', '--table-type', 'both']
+cmd1 = ['C:\\Users\\vilsn\\Documents\\isoform-analysis\\isoform_dashboard_env\\Scripts\\python.exe', '-u', '-m', 'isoform_distribution.distributions', '--matrix', 'C:\\Users\\vilsn\\Documents\\isoform-analysis\\data\\expressed_isoforms_matrix.tsv', '--gtf', 'C:\\Users\\vilsn\\Documents\\isoform-analysis\\data\\expressed_isoforms.gtf', '--meta-file', 'C:\\Users\\vilsn\\Documents\\isoform-analysis\\data\\metadata.tsv', '--output-dir', 'data/isoform_distributions', '--meta-sample-col', 'sample_id', '--meta-group-col', 'condition', '--cutoff-pct', '1.5', '--table-type', 'both']
 cmd2 = ['C:\\Users\\vilsn\\Documents\\isoform-analysis\\isoform_dashboard_env\\Scripts\\python.exe', '-u', '-m', 'isoform_distribution.bootstrap_isoform_means', '--input', 'C:\\Users\\vilsn\\Documents\\isoform-analysis\\data\\expressed_isoforms_matrix.tsv', '--output-dir', 'data/isoform_distributions', '--iterations', '1000', '--seed', '42', '--include-key', 'adult']
 
 print("=== Running Step 1/2: Calculate Distribution Tables ===")
